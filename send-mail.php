c049dd7248047dd844feb992e8508ee4db8f332e
Pedro Landriel <pedrolandriel@live.com.ar>
2022-03-22 08:13:56 -0300

    Se aplicaron cambios y fix para la entrega final del proyecto.

---
+++ send-mail.php
@@ -0,0 +1,19 @@
+<?
+$Name=$_POST['name'];
+$Surname=$_POST['surname'];
+$Email=$_POST['email'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+$body .= "Nombre: " . $Name . "\n";
+$body .= "Email: " . $Email . "\n";
+$body .= "Asunto: " . $subject . "\n";
+$body .= "Mensaje: " . $message . "\n";
+//replace with your email
+mail("info@pergaminoit.com.ar","Del sitio web",$body);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<script>alert("Su mensaje fue enviado. Gracias!");</script>
+<meta HTTP-EQUIV="REFRESH" content="0; url=https://seghma.com.ar/index.html">
+</head>
